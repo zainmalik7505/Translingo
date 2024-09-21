15d5138e40556827c9b9e46ad0f22ae8283a4df1
Zain Malik <xaineil121@gmail.com>
2024-09-21 11:09:59 +0500

    Initial Commit

---
+++ Translingo/TranslingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslingoApp.swift
+//  Translingo
+//
+//  Created by Zain Malik on 21/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TranslingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TranslingoTests/TranslingoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TranslingoTests.swift
+//  TranslingoTests
+//
+//  Created by Zain Malik on 21/09/2024.
+//
+
+import Testing
+@testable import Translingo
+
+struct TranslingoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
